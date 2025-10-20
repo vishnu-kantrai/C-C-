c93dbbd5eb2b69490b3f4aed632161da0774ade5
Vishnu <238951490+vishnu-kantrai@users.noreply.github.com>
2025-10-20 14:25:17 +0530

    Update hello.cpp

--- 01_Basics/hello.cpp
+++ 01_Basics/hello.cpp
@@ -3,6 +3,6 @@ using namespace std;
 
 int main()
 {
- cout<<"Hello,this is my (Vishnu Kant Rai)  first C++ program on GitHub"<<endl
+ cout<<"Hello,this is my (Vishnu Kant Rai) first C++ program on GitHub"<<endl
 return 0;
 }
