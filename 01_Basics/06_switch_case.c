16881e8dac6fc29cd85953a8d1c9311e4684572e
Vishnu <238951490+vishnu-kantrai@users.noreply.github.com>
2025-10-25 19:51:56 +0530

    Create 06_switch_case.c
    This program introduces with switch case. It takes input and displays the day corresponding to it.

---
+++ 01_Basics/06_switch_case.c
@@ -0,0 +1,42 @@
+/*This program introduces switch case. It takes input from user and prints out the day corresponding to that number, starting with 1=Monday*/
+
+#include<stdio.h>
+#include<conio.h>
+
+int main()
+{
+int day;
+printf("Enter number: ");
+scanf(" %d",&day);
+
+switch(day%7)
+{
+case 0: 
+printf("\nSunday");
+break;
+case 1: 
+printf("\nMonday"); 
+break;
+case 2:
+printf("\nTuesday"); 
+break;
+case 3: printf("\nWednesday"); 
+break;
+case 4: 
+printf("\nThursday"); 
+break;
+case 5: 
+printf("\nFriday"); 
+break;
+case 6: 
+printf("\nSaturday"); 
+break;
+default:
+printf("\nInvalid");
+break;
+}
+
+getch();
+return 0;
+
+}
