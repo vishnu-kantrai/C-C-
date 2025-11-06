90bfc31e6fd065088b3dc197d0003069b7967852
Vishnu <238951490+vishnu-kantrai@users.noreply.github.com>
2025-11-06 23:56:16 +0530

    Create 05_Hollow_rectangle.cpp

---
+++ Practice_problems/Patterns/05_Hollow_rectangle.cpp
@@ -0,0 +1,32 @@
+/*A hollow rectangle printing program
+j is used to denote rows
+i is used to denote columns*/
+
+#include<iostream>
+using namespace std;
+
+int main()
+{
+ unsigned int length, width = 0;
+  cout << "Enter Length of rectangle: ";
+  cin >> length;
+  cout << "\n";
+  cout << "Enter Width of rectangle: ";
+  cin >> width;
+
+  for(int j=0; j<width; j++)
+    {
+      for(int i=0; i<length; i++)
+        {
+          if((i==0 || i==length-1) && (j==0 || j==width-1))
+            cout << "* ";
+          else
+            cout << "  ";
+        }
+      cout << "\n";
+    }
+  
+  cin.ignore();
+  cin.get();
+  return 0;
+}
