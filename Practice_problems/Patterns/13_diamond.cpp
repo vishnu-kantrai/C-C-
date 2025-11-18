cef6e07750e752b52cff4b42bf8a4d5e7905028c
Vishnu <238951490+vishnu-kantrai@users.noreply.github.com>
2025-11-18 21:45:58 +0530

    Create 13_diamond.cpp

---
+++ Practice_problems/Patterns/13_diamond.cpp
@@ -0,0 +1,41 @@
+/*A program to print diamond shape*/
+
+#include<iostream>
+using namespace std;
+
+int main()
+{
+  unsigned int rows = 0;
+  cout << "Enter No. of Rows: ";
+  cin >> rows;
+
+  //upright part
+  for(int j=0; j<rows; j++)
+    {
+      //spaces
+      for(int i=1; i<rows-j; i++)
+        cout << " ";
+
+      //stars
+      for(int i=1; i<=2*j+1; i++)
+        cout << "*";
+      cout << endl;
+    }
+  //downside part
+
+  for(int j=rows-1; j>0; j--)
+    {
+      //spaces
+      for(int i=1; i<=rows-j; i++)
+        cout << " ";
+
+      //stars
+      for(int i=1; i<=2*j-1; i++)
+        cout << "*";
+      cout << endl;
+    }
+
+  cin.ignore();
+  cin.get();
+  return 0;
+}
